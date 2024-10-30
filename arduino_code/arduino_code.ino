81e2e198ff26ca8d68e28fb5a51ccd504818d72b
Anthony Aniobi <anthonyaniobi198@gmail.com>
2024-10-30 12:30:27 +0100

    update arduino code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,97 +1,157 @@
-//#include <SoftwareSerial.h>
-//
-////variable for storing incoming values
-//SoftwareSerial bluetooth(0, 1); //RX, TX
-//
-////char Incoming_value = 0;
-//
-//String str_ii = "";
-//int ii_0 = 0;
-//
-//void setup() {
-//  Serial.begin(115200);
-//  delay(1000);
-//  bluetooth.begin(9600);
-//  delay(1000);
-//  bluetooth_help();
-//}
-//
-//void loop() {
-//  // put your main code here, to run repeatedly:
-////  if(Serial.available() > 0){
-////    Icoming_value = Serial.read();
-////    Serial.print(Incoming_value);
-////    Serial.print("\n");
-////
-////    if(Incoming_value == '0'){
-////      digitalWrite(8, LOW);
-////    }else if(Incoming_value == '1'){
-////      digitalWrite(8, HIGH);
-////    }
-////  }
-////  delay(1);
-//}
-//
-//void bluetooth_help(){
-//  bluetooth.println("AT+HELP?");
-//  while(true){
-//    char in_char = bluetooth.read();
-//    if(int(in_char)== -1 or int(in_char)==42){continue;}
-//    str_ii += in_char;
-//    if(in_char == '\n') {
-//      if(str_ii = String('\r')+String('\n')){
-//        if(ii_0 == 0){
-//          ii_0 = 1;
-//          continue;
-//        }
-//        break;
-//      }
-//      Serial.print(str_ii);
-//      str_ii = "";
-//    }
-//  }
-//}
+#include <SoftwareSerial.h>
+#include "bluetooth_commands.hpp"
+#include <Stepper.h>
+#include <Servo.h> 
+ 
+Servo myservo;                  
+int servoPin = 7;
+int servoPos = 0;    
 
+int lightPin = 6;
 
+// Defines the number of steps per rotation
+const int stepsPerRevolution = 2038;
 
-int ledPin = 13;
-int cmd = -1;
-int flag = 0;
+//variable for storing incoming values
+SoftwareSerial bluetoothSerial(0, 1); //RX, TX
+
+// stepper motor library
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+// incoming value from bluetooth
+char incoming_value = 0;
+
+// bluetooth logger
+String str_ii = "";
+int ii_0 = 0;
+
+// variable for storing states
+bool lightIsOn = false;
+bool windowIsOpen = false;
+bool doorIsOpen = false;
+
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(1000);
+  bluetoothSerial.begin(9600);
+  delay(1000);
+  bluetooth_help();
+  // initialize servo
+  myservo.attach(servoPin);
+  pinMode(lightPin, OUTPUT);
+  // initialize values
+  initializeValues();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    cmd = Serial.read();
-    flag = 1;
-  }
-
-  if (flag == 1) {
-    if (cmd == '0') {
-      digitalWrite(ledPin, LOW);
-      Serial.println("LED: off");
-    } else if (cmd == '1') {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("LED: on");
-    } else {
-      Serial.print("unknown command: ");
-      Serial.write(cmd);
-      Serial.print(" (");
-      Serial.print(cmd, DEC);
-      Serial.print(")");
-
-      Serial.println();
+  // if bluetooth serial is available 
+  // run the code 
+  if(bluetoothSerial.available() > 0){
+    incoming_value = bluetoothSerial.read();
+    // log bluetooth value in the serial monitor
+    Serial.print("Value gotten from the bluetooth is: ");
+    Serial.println(incoming_value);
+    
+    switch (incoming_value){
+        case OPEN_WINDOW:
+          openWindow();
+          break;
+        case CLOSE_WINDOW:
+          closeWindow();
+          break;
+        case OPEN_DOOR:
+          openDoor();
+          break;
+        case CLOSE_DOOR:
+          closeDoor();
+          break;
+        case TURN_ON_LIGHT:
+          turnOffLight();
+          break;
+        case TURN_OFF_LIGHT:
+          turnOnLight();
+          break;
+        default:
+          break;
     }
-
-    flag = 0;    
-    cmd = 65;
   }
+}
 
-  Serial.flush();
-  delay(100);
+void openDoor(){
+  if(!doorIsOpen){
+    // Rotate clockwise at 8RPM
+    myStepper.setSpeed(8);
+    myStepper.step(stepsPerRevolution);
+    doorIsOpen = true;
+  }
+}
+
+void closeDoor(){
+  if(doorIsOpen){ 
+    // Rotate anti-clockwise at 8RPM
+    myStepper.setSpeed(8);
+    myStepper.step(-stepsPerRevolution);
+    doorIsOpen = false;
+  }
+}
+
+void openWindow(){
+  if(!windowIsOpen){
+    for(servoPos = 0; servoPos <= 180; servoPos += 1){                                  
+      myservo.write(servoPos);      
+      delay(15);                    
+    } 
+    windowIsOpen = true;
+  }
+}
+
+void closeWindow(){
+  if(windowIsOpen){
+    for(servoPos = 180; servoPos >= 0; servoPos -= 1){                                  
+      myservo.write(servoPos);      
+      delay(15);                
+    }
+    windowIsOpen = false;
+  }
+}
+
+void turnOnLight(){
+  if(!lightIsOn){
+    digitalWrite(lightPin, HIGH);
+    lightIsOn = true;
+  }
+}
+
+void turnOffLight(){
+  if(lightIsOn){
+    digitalWrite(lightPin, LOW);
+    lightIsOn = false;
+  }
+}
+
+void initializeValues(){
+  myservo.write(0);
+  digitalWrite(lightPin, LOW);
+}
+
+void bluetooth_help(){
+ bluetoothSerial.println("AT+HELP?");
+ while(true){
+   char in_char = bluetoothSerial.read();
+   if(int(in_char)== -1 or int(in_char)==42){continue;}
+   str_ii += in_char;
+   if(in_char == '\n') {
+     if(str_ii = String('\r')+String('\n')){
+       if(ii_0 == 0){
+         ii_0 = 1;
+         continue;
+       }
+       break;
+     }
+     Serial.print(str_ii);
+     str_ii = "";
+   }
+ }
 }
 
---
+++ arduino_code/bluetooth_commands.hpp
@@ -0,0 +1,9 @@
+
+/* Commands for bluetooth*/
+
+static const char OPEN_WINDOW = 'o';
+static const char CLOSE_WINDOW = 'c';
+static const char OPEN_DOOR = 'p';
+static const char CLOSE_DOOR = 'd';
+static const char TURN_ON_LIGHT = 't';
+static const char TURN_OFF_LIGHT = 'f';
